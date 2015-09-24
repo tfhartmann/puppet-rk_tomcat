ba6839cee2a293a84d3c6434babdd234fcb83ac5
Steve Huff <steve.huff@runkeeper.com>
2015-09-24 16:38:10 -0400

    Initial commit

---
+++ scripts/deploy.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+#
+if [[ "${USER}" -ne 0 ]]; then
+  echo "$0 must be run as root."
+  exit 1
+fi
+
+echo "### Patching system..."
+yum -y update
+
+echo "### Uninstalling upstream Puppet..."
+yum -y erase puppet
+
+echo "### Installing git..."
+yum -y install git
+
+cd ~
+
+echo "### Cloning Tomcat platform configuration..."
+git clone https://github.com/FitnessKeeper/puppet-rk_tomcat.git rk_tomcat
+
+cd rk_tomcat
+
+echo "### Configuring RubyGems..."
+echo '--nodocument --bindir /usr/local/bin' >> /root/.gemrc
+
+echo "### Installing Bundler..."
+gem install bundler
+
+echo "### Installing other gem dependencies..."
+bundle install
+
+echo "### Installing Puppet dependencies..."
+librarian-puppet install
+
+echo "### Running Puppet agent..."
+puppet apply --modulepath "modules:${HOME}" -e 'class { "rk_tomcat": }'
+
+echo "### Disabling Puppet agent..."
+puppet resource service puppet ensure=stopped enable=false
