48a4cf0ececf9b28553a6b2cb9a51d44579bd171
Steve Huff <steve.huff@runkeeper.com>
2015-09-30 13:34:29 -0400

    was this the earlier bug?

--- manifests/java.pp
+++ manifests/java.pp
@@ -7,10 +7,18 @@ class rk_tomcat::java (
 
   # building variables
   $zulu_rpm = "zulu${zulu_version}-x86lx64.rpm"
-  $zulu_rpm_path = "/root/rk_tomcat/files/${zulu_rpm}"
+  # $zulu_rpm_path = "/root/rk_tomcat/files/${zulu_rpm}"
+  $zulu_rpm_path = "/root/${zulu_rpm}"
 
   # install Zulu
-  # this is an awful hack
+  file { 'zulu_rpm':
+    path   => $zulu_rpm_path,
+    owner  => 'root',
+    group  => 'root',
+    mode   => '0644',
+    source => "puppet:///modules/rk_tomcat/${zulu_rpm}",
+  } ->
+
   exec { 'install_zulu_rpm':
     path      => '/bin:/usr/bin:/sbin:/usr/sbin',
     command   => "yum -y localinstall $zulu_rpm_path",
