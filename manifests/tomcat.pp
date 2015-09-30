e4e8e89b1846d8f3c5c0479e2c4b2abae1b1ac03
Steve Huff <steve.huff@runkeeper.com>
2015-09-30 09:41:47 -0400

    MessageQueueingConfiguration.conf

--- manifests/tomcat.pp
+++ manifests/tomcat.pp
@@ -2,6 +2,7 @@
 #
 class rk_tomcat::tomcat (
   $artifacts,
+  $aws_keys,
   $catalina_home,
   $cloudant_user,
   $cloudant_password,
@@ -9,6 +10,9 @@ class rk_tomcat::tomcat (
   $deploy_user,
   $deploy_password,
   $logentries_tokens,
+  $redis_queue_host,
+  $redis_queue_port,
+  $redis_queue_db,
   $tomcat_instance,
   $tomcat_pkg,
   $tomcat_svc,
@@ -20,15 +24,20 @@ class rk_tomcat::tomcat (
   if ( $staging_instance ) {
     $cloudant_suffix = "-${staging_instance}"
     $log_identifier = $staging_instance
+    $queue_identifier = $staging_instance
   }
   else {
     $cloudant_suffix = ''
     $log_identifiers = $artifacts.map |$pair| { $pair[0] }
     $log_identifier = $log_identifiers[0]
+    $queue_identifier = ''
   }
 
   $logentries_analytics_token = $logentries_tokens['analytics']
   $logentries_applogs_token = $logentries_tokens['applogs']
+
+  $sqs_access_key = $aws_keys['sqs']['access_key']
+  $sqs_secret_key = $aws_keys['sqs']['secret_key']
 
   File {
     ensure => 'present',
@@ -64,6 +73,11 @@ class rk_tomcat::tomcat (
   file { 'logbackInclude.xml':
     path    => "${catalina_home}/conf/logbackInclude.xml",
     content => template('rk_tomcat/logbackInclude.xml.erb'),
+  } ->
+
+  file { 'MessageQueueingConfiguration.conf':
+    path    => "${catalina_home}/conf/MessageQueueingConfiguration.conf",
+    content => template('rk_tomcat/MessageQueueingConfiguration.conf.erb'),
   } ->
 
   ::tomcat::service { $tomcat_instance:
