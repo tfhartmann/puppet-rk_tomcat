5935b9ba4c78010218c25bafd55e0d9e46f89656
Steve Huff <steve.huff@runkeeper.com>
2015-10-07 15:27:17 -0400

    more utility scripts

---
+++ files/imageGoldMaster.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+#
+# Make an image of the running gold master instance.
+
+if [ -r ".env" ]; then
+  . .env
+else
+  echo "Populate .env first."
+  exit 1
+fi
+
+# find the gold master instance
+INSTANCE_ID=$(aws ec2 describe-instances --filters "Name=tag:Name,Values=tomcat7-gold-master" "Name=instance-state-name,Values=running" | jq -r '.Reservations[].Instances[].InstanceId')
+
+# create the image
+IMAGE_INDEX=$(aws ec2 describe-images --owners self | jq -r '.Images | map(select(.Name | startswith("tomcat7-master-"))) | sort_by(.Name) | last | .Name | ltrimstr("tomcat7-master-")')
+let IMAGE_INDEX++
+
+IMAGE_NAME="tomcat7-master-${IMAGE_INDEX}"
+
+IMAGE_ID=$(aws ec2 create-image --instance-id $INSTANCE_ID --name $IMAGE_NAME --reboot | jq -r '.ImageId')
+echo $IMAGE_ID
+
+if [ -z "$IMAGE_ID" ]; then
+  exit 1
+fi
+
+IMAGE_STATE=''
+while [ "$IMAGE_STATE" != "available" ]; do
+  sleep 2
+  IMAGE_STATE=$(aws ec2 describe-images --image-ids $IMAGE_ID --owners self | jq -r '.Images[].State')
+done
+echo $IMAGE_STATE
---
+++ files/instantiateDeploy.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+#
+# Spin up an instance to make a new deploy image.
+
+if [ -r ".env" ]; then
+  . .env
+else
+  echo "Populate .env first."
+  exit 1
+fi
+
+# create instance
+GOLD_MASTER_AMI=$(aws ec2 describe-images --owners self | jq -r '.Images | map(select(.Name | startswith("tomcat7-master-"))) | sort_by(.Name) | last | .ImageId')
+
+INSTANCE_DATA=$(aws ec2 run-instances \
+  --image-id "$GOLD_MASTER_AMI" \
+  --key-name "$BUILD_SSH_KEYPAIR" \
+  --security-group-ids "$BUILD_SECURITY_GROUP" \
+  --instance-type "$BUILD_INSTANCE_TYPE" \
+  --subnet-id "$BUILD_SUBNET" \
+  --iam-instance-profile "Name=${BUILD_PROFILE_NAME}")
+
+INSTANCE_ID=$(echo $INSTANCE_DATA | jq -r '.Instances[].InstanceId')
+sleep 5
+
+# tag instance
+IMAGE_INDEX=$(aws ec2 describe-images --owners self | jq -r '.Images | map(select(.Name | startswith("tomcat7-stage21-"))) | sort_by(.Name) | last | .Name | ltrimstr("tomcat7-stage21-")')
+let IMAGE_INDEX++
+aws ec2 create-tags --resources $INSTANCE_ID --tags "Key=Name,Value=tomcat7-stage21-${IMAGE_INDEX}"
+
+echo $INSTANCE_ID
+INSTANCE_HOSTNAME=''
+
+# wait for hostname
+while [ -z "$INSTANCE_HOSTNAME" ]; do
+  sleep 2
+  INSTANCE_HOSTNAME=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID | jq -r '.Reservations[].Instances[].PrivateDnsName')
+
+  if [ "$INSTANCE_HOSTNAME" = "null" ]; then
+    INSTANCE_HOSTNAME=''
+  fi
+done
+
+# wait for the instance to be up
+INSTANCE_STATE=''
+while [ "$INSTANCE_STATE" != 'running' ]; do
+  sleep 2
+  INSTANCE_STATE=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID | jq -r '.Reservations[].Instances[].State.Name')
+done
+
+echo $INSTANCE_HOSTNAME
