38a06689d9d190292618e21976993628c98a8f08
Steve Huff <steve.huff@runkeeper.com>
2015-11-09 14:22:35 -0500

    allow passing an argument to storeSecrets.sh

--- files/storeSecrets.sh
+++ files/storeSecrets.sh
@@ -4,11 +4,19 @@
 
 SECRETS_FILE='data/secrets.yaml'
 REGION='us-east-1'
-TARGET="s3://rk-devops-${REGION}/secrets/secrets.yaml"
 
 if [ ! -r "$SECRETS_FILE" ]; then
   echo "Unable to read '$SECRETS_FILE', exiting."
   exit 1
 fi
+
+if [ -n "$1" ]; then
+  TAG="$1"
+  TARGET_FILE="secrets-${TAG}.yaml"
+else
+  TARGET_FILE="secrets.yaml"
+fi
+
+TARGET="s3://rk-devops-${REGION}/secrets/${TARGET_FILE}"
 
 aws s3 cp $SECRETS_FILE $TARGET
