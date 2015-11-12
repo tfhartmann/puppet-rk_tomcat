690a1e71b744329f273cc619629d680758f35bcc
Steve Huff <steve.huff@runkeeper.com>
2015-11-12 10:01:54 -0500

    more New Relic

--- files/instantiateDeploy.sh
+++ files/instantiateDeploy.sh
@@ -11,15 +11,21 @@ fi
 
 echo "Starting instance for '$BUILD_TARGET' image."
 
+# look up resource IDs
+BUILD_VPC_ID=$(aws ec2 describe-vpcs --filters "Name=tag:Name,Values=${BUILD_VPC}" | jq -r '.Vpcs | last | .VpcId')
+SG_FILTER=$(echo -n '.SecurityGroups | map({GroupName, GroupId}) | map(select(.GroupName | test("' && echo -n "^${BUILD_VPC}-${BUILD_SECURITY_GROUP}-.*$" && echo -n '")))[] | .GroupId')
+BUILD_SECURITY_GROUP_ID=$(aws ec2 describe-security-groups --filters "Name=vpc-id,Values=${BUILD_VPC_ID}" | jq -r "$SG_FILTER")
+BUILD_SUBNET_ID=$(aws ec2 describe-subnets --filters "Name=vpc-id,Values=${BUILD_VPC_ID}" "Name=tag:Name,Values=${BUILD_SUBNET}" | jq -r '.Subnets | last | .SubnetId')
+
 # create instance
 GOLD_MASTER_AMI=$(aws ec2 describe-images --owners self | jq -r '.Images | map(select(.Name | startswith("tomcat7-master-"))) | sort_by(.CreationDate) | last | .ImageId')
 
 INSTANCE_DATA=$(aws ec2 run-instances \
   --image-id "$GOLD_MASTER_AMI" \
   --key-name "$BUILD_SSH_KEYPAIR" \
-  --security-group-ids "$BUILD_SECURITY_GROUP" \
+  --security-group-ids "$BUILD_SECURITY_GROUP_ID" \
   --instance-type "$BUILD_INSTANCE_TYPE" \
-  --subnet-id "$BUILD_SUBNET" \
+  --subnet-id "$BUILD_SUBNET_ID" \
   --iam-instance-profile "Name=${BUILD_PROFILE_NAME}")
 
 INSTANCE_ID=$(echo $INSTANCE_DATA | jq -r '.Instances[].InstanceId')
