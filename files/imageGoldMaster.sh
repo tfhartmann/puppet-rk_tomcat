839cf898e43961ebd08d9b1e50b0ab258a7fb974
Steve Huff <steve.huff@runkeeper.com>
2015-11-16 14:34:58 -0500

    explicitly pass region to aws cli

--- files/imageGoldMaster.sh
+++ files/imageGoldMaster.sh
@@ -9,13 +9,17 @@ else
   exit 1
 fi
 
-STATE='.env'
-if [ -r "$STATE" ]; then
-  . "$STATE"
-else
-  echo "Unable to read state file, querying AWS to determine gold master instance."
+# determine region
+if [ -z "$REGION" ]; then
+  REGION=us-east-1
+fi
+
+AWS="aws --region $REGION"
+
+if [ -z "$INSTANCE_ID" ]; then
+  echo "Querying AWS to determine gold master instance."
   # find the gold master instance
-  INSTANCE_ID=$(aws ec2 describe-instances --filters "Name=tag:Name,Values=tomcat7-gold-master" "Name=instance-state-name,Values=running" | jq -r '.Reservations[].Instances[].InstanceId')
+  INSTANCE_ID=$($AWS ec2 describe-instances --filters "Name=tag:Name,Values=tomcat7-gold-master" "Name=instance-state-name,Values=running" | jq -r '.Reservations[].Instances[].InstanceId')
 fi
 
 if [ -z "$INSTANCE_ID" ]; then
@@ -24,12 +28,12 @@ if [ -z "$INSTANCE_ID" ]; then
 fi
 
 # create the image
-IMAGE_INDEX=$(aws ec2 describe-images --owners self | jq -r '.Images | map(select(.Name | startswith("tomcat7-master-"))) | sort_by(.CreationDate) | last | .Name | ltrimstr("tomcat7-master-")')
+IMAGE_INDEX=$($AWS ec2 describe-images --owners self | jq -r '.Images | map(select(.Name | startswith("tomcat7-master-"))) | sort_by(.CreationDate) | last | .Name | ltrimstr("tomcat7-master-")')
 let IMAGE_INDEX++
 
 IMAGE_NAME="tomcat7-master-${IMAGE_INDEX}"
 
-IMAGE_ID=$(aws ec2 create-image --instance-id $INSTANCE_ID --name $IMAGE_NAME --reboot | jq -r '.ImageId')
+IMAGE_ID=$($AWS ec2 create-image --instance-id $INSTANCE_ID --name $IMAGE_NAME --reboot | jq -r '.ImageId')
 echo $IMAGE_ID
 
 if [ -z "$IMAGE_ID" ]; then
@@ -39,11 +43,11 @@ fi
 IMAGE_STATE=''
 while [ "$IMAGE_STATE" != "available" ]; do
   sleep 2
-  IMAGE_STATE=$(aws ec2 describe-images --image-ids $IMAGE_ID --owners self | jq -r '.Images[].State')
+  IMAGE_STATE=$($AWS ec2 describe-images --image-ids $IMAGE_ID --owners self | jq -r '.Images[].State')
 done
 echo $IMAGE_STATE
 
-TERMINATED_INSTANCE_ID=$(aws ec2 terminate-instances --instance-ids $INSTANCE_ID | jq -r '.TerminatingInstances[].InstanceId')
+TERMINATED_INSTANCE_ID=$($AWS ec2 terminate-instances --instance-ids $INSTANCE_ID | jq -r '.TerminatingInstances[].InstanceId')
 
 # save state for the next script
 echo > $STATE <<STATE
--- files/instantiateGoldMaster.sh
+++ files/instantiateGoldMaster.sh
@@ -11,14 +11,21 @@ fi
 
 STATE='.state'
 
+# determine region
+if [ -z "$REGION" ]; then
+  REGION=us-east-1
+fi
+
+AWS="aws --region $REGION"
+
 # look up resource IDs
-BUILD_VPC_ID=$(aws ec2 describe-vpcs --filters "Name=tag:Name,Values=${BUILD_VPC}" | jq -r '.Vpcs | last | .VpcId')
+BUILD_VPC_ID=$($AWS ec2 describe-vpcs --filters "Name=tag:Name,Values=${BUILD_VPC}" | jq -r '.Vpcs | last | .VpcId')
 SG_FILTER=$(echo -n '.SecurityGroups | map({GroupName, GroupId}) | map(select(.GroupName | test("' && echo -n "^${BUILD_VPC}-${BUILD_SECURITY_GROUP}-.*$" && echo -n '")))[] | .GroupId')
-BUILD_SECURITY_GROUP_ID=$(aws ec2 describe-security-groups --filters "Name=vpc-id,Values=${BUILD_VPC_ID}" | jq -r "$SG_FILTER")
-BUILD_SUBNET_ID=$(aws ec2 describe-subnets --filters "Name=vpc-id,Values=${BUILD_VPC_ID}" "Name=tag:Name,Values=${BUILD_SUBNET}" | jq -r '.Subnets | last | .SubnetId')
+BUILD_SECURITY_GROUP_ID=$($AWS ec2 describe-security-groups --filters "Name=vpc-id,Values=${BUILD_VPC_ID}" | jq -r "$SG_FILTER")
+BUILD_SUBNET_ID=$($AWS ec2 describe-subnets --filters "Name=vpc-id,Values=${BUILD_VPC_ID}" "Name=tag:Name,Values=${BUILD_SUBNET}" | jq -r '.Subnets | last | .SubnetId')
 
 # create instance
-INSTANCE_DATA=$(aws ec2 run-instances \
+INSTANCE_DATA=$($AWS ec2 run-instances \
   --image-id "$AWS_LINUX_AMI" \
   --key-name "$BUILD_SSH_KEYPAIR" \
   --security-group-ids "$BUILD_SECURITY_GROUP_ID" \
@@ -30,7 +37,7 @@ INSTANCE_ID=$(echo $INSTANCE_DATA | jq -r '.Instances[].InstanceId')
 
 # tag instance
 sleep 5
-aws ec2 create-tags --resources $INSTANCE_ID --tags "Key=Name,Value=tomcat7-gold-master"
+$AWS ec2 create-tags --resources $INSTANCE_ID --tags "Key=Name,Value=tomcat7-gold-master"
 
 echo $INSTANCE_ID
 INSTANCE_HOSTNAME=''
@@ -38,7 +45,7 @@ INSTANCE_HOSTNAME=''
 # wait for hostname
 while [ -z "$INSTANCE_HOSTNAME" ]; do
   sleep 2
-  INSTANCE_HOSTNAME=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID | jq -r '.Reservations[].Instances[].PrivateDnsName')
+  INSTANCE_HOSTNAME=$($AWS ec2 describe-instances --instance-ids $INSTANCE_ID | jq -r '.Reservations[].Instances[].PrivateDnsName')
 
   if [ "$INSTANCE_HOSTNAME" = "null" ]; then
     INSTANCE_HOSTNAME=''
@@ -49,7 +56,7 @@ done
 INSTANCE_STATE=''
 while [ "$INSTANCE_STATE" != 'running' ]; do
   sleep 2
-  INSTANCE_STATE=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID | jq -r '.Reservations[].Instances[].State.Name')
+  INSTANCE_STATE=$($AWS ec2 describe-instances --instance-ids $INSTANCE_ID | jq -r '.Reservations[].Instances[].State.Name')
 done
 
 echo $INSTANCE_HOSTNAME
