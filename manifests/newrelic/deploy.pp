18aec1a9ff118a396182bd98d8aff658508d04ba
Joe Bondi <joe.bondi@runkeeper.com>
2015-11-18 16:04:36 -0500

    Added new relic agent to tomcat7.conf java startup options

--- manifests/newrelic/deploy.pp
+++ manifests/newrelic/deploy.pp
@@ -1,25 +1,11 @@
 # rk_tomcat::newrelic::deploy
 #
 class rk_tomcat::newrelic::deploy(
-  $app_names,
-  $artifacts,
+  $app_name,
   $attr_include,
   $attr_exclude,
   $license,
 ) inherits rk_tomcat::newrelic {
-  if ( size($artifacts) == 1 ) {
-    $newrelic_environment = 'production'
-    if has_key($app_names, $artifacts[0]) {
-      $app_name = $app_names[$artifacts[0]]
-    }
-    else {
-      $app_name = 'Unknown'
-    }
-  }
-  else {
-    $newrelic_environment = 'staging'
-    $app_name = 'Runkeeper Applications'
-  }
 
   file { "$newrelic_dir/newrelic.yml":
     ensure  => present,
