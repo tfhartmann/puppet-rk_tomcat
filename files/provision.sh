74933121a833ba293fdf0c8b7bd3badae0da3a2b
Steve Huff <steve.huff@runkeeper.com>
2015-11-18 09:21:54 -0500

    Revert "is logdest the problem?"
    This reverts commit efbb8aaf167c0e3f484f1badb050a847cbe2ea8d.

--- files/provision.sh
+++ files/provision.sh
@@ -84,8 +84,7 @@ HIERA
 mkdir -p /var/log/puppet
 
 PUPPET=$(which puppet 2>/dev/null || echo '/usr/local/bin/puppet')
-echo "PUPPET: $PUPPET"
-$PUPPET apply --hiera_config "/etc/hiera/hiera.yaml" --modulepath "$(pwd)/modules:/etc/puppetlabs/code/modules" -e 'class { "rk_tomcat": mode => "provision" }'
+$PUPPET apply --hiera_config "/etc/hiera/hiera.yaml" --modulepath "$(pwd)/modules:/etc/puppetlabs/code/modules" --logdest /var/log/puppet/provision.log -e 'class { "rk_tomcat": mode => "provision" }'
 
 $LOGGER "Disabling Puppet agent..."
 $PUPPET resource service puppet ensure=stopped enable=false
