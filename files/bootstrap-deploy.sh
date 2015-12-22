28b07557a816f1805c809f5351cba7a126de4500
Steve Huff <steve.huff@runkeeper.com>
2015-12-22 12:53:26 -0500

    AHA, forgot to ever download the semaphore when imaging a deploy :facepalm:

--- files/bootstrap-deploy.sh
+++ files/bootstrap-deploy.sh
@@ -9,11 +9,35 @@ if [ -r "/etc/profile.d/aws-apitools-common.sh" ]; then
   . /etc/profile.d/aws-apitools-common.sh
 fi
 
-if [ -x "$THE_SCRIPT" ]; then
-  $LOGGER "Executing '$THE_SCRIPT'"
-  cd /root && "$THE_SCRIPT" > "${THE_SCRIPT}.log" 2>&1
-  exit
-else
-  $LOGGER "'$THE_SCRIPT' not executable, exiting."
-  exit 1
-fi
+AZ=$(ec2-metadata --availability-zone | awk '{print $2}')
+REGION=$(echo "${AZ}" | sed 's/[[:alpha:]]$//')
+AWS="aws --region $REGION"
+
+INSTANCE_ID=$(ec2-metadata --instance-id | awk '{print $2}')
+
+$LOGGER "Bootstrapping instance ${INSTANCE_ID}"
+
+TRIES=0
+LIMIT=6
+
+SOURCE="s3://rk-devops-${REGION}/jenkins/semaphores/${INSTANCE_ID}"
+TARGET="$THE_SCRIPT"
+
+while [ "$TRIES" -lt "$LIMIT" ]; do
+  $LOGGER "Downloading '$TARGET' from '$SOURCE' [try $TRIES]"
+
+  $AWS s3 cp "$SOURCE" "$TARGET" 2>/dev/null
+
+  if [ -r "$THE_SCRIPT" ]; then
+    $LOGGER "Download successful, executing '$THE_SCRIPT'"
+    cd $(dirname "$THE_SCRIPT") && chmod +x "$THE_SCRIPT" && "$THE_SCRIPT" > "${THE_SCRIPT}.log" 2>&1
+    exit
+  else
+    $LOGGER "No semaphore found, sleeping..."
+    let TRIES++
+    sleep 10
+  fi
+done
+
+$LOGGER "No semaphore found before timeout, exiting."
+exit 1
