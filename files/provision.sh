1e9b4f3650d862724b74066dbd947a0ead01d515
Steve Huff <steve.huff@runkeeper.com>
2016-01-04 15:15:37 -0500

    all the Goss tooling for the provisioning stage

--- files/provision.sh
+++ files/provision.sh
@@ -128,6 +128,18 @@ $PUPPET resource service puppet ensure=stopped enable=false
 $LOGGER "Linking Tomcat homedir to CATALINA_HOME..."
 ln -s /usr/share/tomcat7 /home/tomcat
 
+$LOGGER "Testing configuration with Goss..."
+GOSS_OUT=/root/goss.out
+goss render && goss validate > $GOSS_OUT
+
+if [ -r "$GOSS_OUT" ]; then
+  cat $GOSS_OUT
+  $LOGGER "Uploading Goss test results to S3..."
+  $AWS s3 cp $GOSS_OUT "s3://rk-devops-${REGION}/jenkins/tests/${INSTANCE_ID}"
+else
+  $LOGGER "No Goss results found!  Not uploading."
+fi
+
 $LOGGER "Removing semaphore..."
 $AWS s3 rm "s3://rk-devops-${REGION}/jenkins/semaphores/${INSTANCE_ID}" 2>/dev/null || true
 
--- manifests/goss.pp
+++ manifests/goss.pp
@@ -3,6 +3,15 @@
 class rk_tomcat::goss (
   $version,
   $destination,
+  $catalina_home,
+  $postgres_driver,
+  $tomcat_pkg,
+  $tomcat_native_pkg,
+  $tomcat_svc,
+  $tomcat_user,
+  $tomcat_group,
+  $zulu_package,
+  $zulu_version,
 ) {
   wget::fetch { "download_goss":
     source      => "https://github.com/aelsabbahy/goss/releases/download/${version}/goss-linux-amd64",
@@ -14,5 +23,21 @@ class rk_tomcat::goss (
     group  => 'root',
     mode   => '0755',
     backup => false,
+  } ->
+
+  file { '/root/goss_provision.json':
+    ensure  => 'present',
+    owner   => 'root',
+    group   => 'root',
+    mode    => '0644',
+    content => template('rk_tomcat/goss_provision.json.erb'),
+  } ->
+
+  file { '/root/goss.json':
+    ensure  => 'present',
+    owner   => 'root',
+    group   => 'root',
+    mode    => '0644',
+    content => template('rk_tomcat/goss.json.erb'),
   }
 }
